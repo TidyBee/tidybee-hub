1a2092b3ad6fff2308b84f1045b50baf503bc42d
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-10-04 08:40:39 +0200

    hub(AgentAuthentication): fill ConnectionModel class file

--- Models/AgentMetadataModel.cs
+++ Models/AgentMetadataModel.cs
@@ -2,7 +2,6 @@ namespace api.Models {
 
     public class AgentMetadataModel
     {
-        public string Address { get; set; }
-        public uint Port { get; set; }
+        
     }
 }
--- Models/ConnectionModel.cs
+++ Models/ConnectionModel.cs
@@ -2,5 +2,7 @@ namespace api.Models {
 
     public class ConnectionModel
     {
+        public string Address { get; set; }
+        public uint Port { get; set; }
     }
 }
