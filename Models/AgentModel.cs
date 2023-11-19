7cb843fdf5c273bedce0890e7d354902832464fa
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-11-19 09:09:17 +0100

    <agentAuth>(Database): Update Agent models for Database integration

--- Models/AgentModel.cs
+++ Models/AgentModel.cs
@@ -1,7 +1,11 @@
-namespace api.Models {
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Models
+{
 
     public class AgentModel
     {
+        [Key]
         public Guid Uuid { get; }
         public AgentStatusModel Status { get; set; }
         public DateTime LastPing { get; set; } // Will be change by the issue #6
