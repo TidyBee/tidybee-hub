55ee958e84ef6b12dcd6bc04c32fd44c74b18646
Lucas Tesnier <lucas.tesnier@epitech.eu>
2023-11-19 09:10:21 +0100

    <agentAuth>(Database): Integrate new change inside project

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using tidybee_hub.Context;
+using tidybee_hub.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DatabaseConnection")));
+builder.Services.AddScoped<AgentRepository>();
 
 var app = builder.Build();
 
@@ -11,6 +18,24 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+if (app.Configuration.GetValue<bool>("EnableAutoMigration"))
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var dbContext = services.GetRequiredService<DatabaseContext>();
+        dbContext.Database.EnsureCreated();
+        dbContext.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
 }
 
 app.UseMiddleware<ProxyMiddleware>();
