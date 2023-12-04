408bf27391f4f8637633138de85c8c9fb544c6a7
Blynqs <baptiste.friboulet@epitech.eu>
2023-12-04 19:44:28 +0900

    architecture refactoring: add aspnet.core proxy

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,4 +1,11 @@
+using AspNetCore.Proxy;
+using Microsoft.Extensions.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -13,6 +20,22 @@ app.UseSwaggerUI();
 
 // app.UseHttpsRedirection();
 app.UseAuthorization();
+
+
+var AgentURL = app.Configuration.GetValue<Uri>("AgentURL");
+
+app.UseProxies(proxies => {
+    proxies.Map("/proxy/{query}", proxy => proxy.UseHttp((context, args) => {
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        foreach (var arg in args)
+        {
+            logger.LogInformation($"Key: {arg.Key}, Value: {arg.Value}");
+        }
+        string? query = args["query"].ToString();
+        return $"{AgentURL}{query}";
+    }));
+});
+
 app.MapControllers();
 
 app.Run();
