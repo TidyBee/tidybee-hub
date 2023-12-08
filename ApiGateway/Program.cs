264876e349a8aeeedbad4401b034ea1498870326
Blynqs <baptiste.friboulet@epitech.eu>
2023-12-08 16:59:21 +0900

    architecture refactoring: add logger in the proxy

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,4 +1,5 @@
 using AspNetCore.Proxy;
+using Microsoft.AspNetCore.Http.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,19 +23,34 @@ builder.Services.AddHttpClient("DataProcessingServiceClient", client =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
 // app.UseHttpsRedirection();
-app.UseAuthorization();
 
 var AgentURL = app.Configuration.GetValue<Uri>("AgentURL");
 
-app.UseProxies(proxies => {
-    proxies.Map("/proxy/{query}", proxy => proxy.UseHttp((context, args) => {
+app.UseProxies(proxies =>
+{
+    proxies.Map("/proxy/{query}", proxy => proxy.UseHttp(async (context, args) =>
+    {
         var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+
+        logger.LogInformation($"Method: {context.Request.Method}, URL: {context.Request.GetDisplayUrl()}");
+        foreach (var header in context.Request.Headers)
+        {
+            logger.LogInformation($"Header: {header.Key}, Value: {header.Value}");
+        }
+
+        if (context.Request.ContentLength > 0 && (context.Request.Method == HttpMethods.Post || context.Request.Method == HttpMethods.Put))
+        {
+            context.Request.EnableBuffering();
+            var bodyReader = new StreamReader(context.Request.Body);
+            var bodyContent = await bodyReader.ReadToEndAsync();
+            context.Request.Body.Position = 0;
+            logger.LogInformation($"Body: {bodyContent}");
+        }
+
         foreach (var arg in args)
         {
             logger.LogInformation($"Key: {arg.Key}, Value: {arg.Value}");
@@ -43,6 +59,8 @@ app.UseProxies(proxies => {
         return $"{AgentURL}{query}";
     }));
 });
+
+app.UseAuthorization();
 
 app.MapControllers();
 
