2067310966408db63205cc7f7c17fa5f51126a41
Guillaume <guillaume.terriere@epitech.eu>
2023-12-28 22:25:30 +0000

    add custom proxy doing same job as previous one

--- Middlewares/ProxyMiddleware.cs
+++ Middlewares/ProxyMiddleware.cs
@@ -2,25 +2,71 @@
 {
     private readonly RequestDelegate _next;
     private readonly HttpClient _httpClient;
+    private readonly Uri _agentURL;
 
-    public ProxyMiddleware(RequestDelegate next)
+    public ProxyMiddleware(RequestDelegate next, IConfiguration configuration)
     {
         _next = next;
-        _httpClient = new HttpClient();
+        var handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true // NOT IN PRODUCTION
+        };
+        _httpClient = new HttpClient(handler);
+        _agentURL = new Uri(configuration.GetValue<string>("AgentURL"));
     }
 
     public async Task InvokeAsync(HttpContext context)
     {
-        string? remoteIpAddress = context.Connection.RemoteIpAddress?.ToString();
-        string? userAgent = context.Request.Headers["User-Agent"];
-        string? requestMethod = context.Request.Method;
-        string? requestPath = context.Request.Path;
+        var remoteIpAddress = context.Connection.RemoteIpAddress?.ToString();
+        var userAgent = context.Request.Headers["User-Agent"];
+        var requestMethod = context.Request.Method;
+        var requestPath = context.Request.Path;
 
         Console.WriteLine($"Request from IP: {remoteIpAddress}");
         Console.WriteLine($"User-Agent: {userAgent}");
         Console.WriteLine($"HTTP Method: {requestMethod}");
         Console.WriteLine($"Request Path: {requestPath}");
 
-        await _next(context);
+        if (requestPath.StartsWithSegments("/proxy"))
+        {
+            var targetUri = new Uri(_agentURL, requestPath.Value.Substring(6));
+
+            var requestMessage = new HttpRequestMessage();
+
+            if (!HttpMethods.IsGet(requestMethod) && !HttpMethods.IsHead(requestMethod))
+            {
+                var streamContent = new StreamContent(context.Request.Body);
+                requestMessage.Content = streamContent;
+            }
+
+            foreach (var header in context.Request.Headers)
+            {
+                requestMessage.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+            }
+
+            requestMessage.RequestUri = targetUri;
+            requestMessage.Method = new HttpMethod(requestMethod);
+
+            var responseMessage = await _httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted);
+
+            context.Response.StatusCode = (int)responseMessage.StatusCode;
+            foreach (var header in responseMessage.Headers)
+            {
+                context.Response.Headers[header.Key] = header.Value.ToArray();
+            }
+
+            foreach (var header in responseMessage.Content.Headers)
+            {
+                context.Response.Headers[header.Key] = header.Value.ToArray();
+            }
+
+            context.Response.Headers.Remove("transfer-encoding");
+
+            await responseMessage.Content.CopyToAsync(context.Response.Body);
+        }
+        else
+        {
+            await _next(context);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ if (app.Configuration.GetValue<bool>("EnableAutoMigration"))
     }
 }
 
-// app.UseMiddleware<ProxyMiddleware>();
+app.UseMiddleware<ProxyMiddleware>();
 
 app.UseStaticFiles();
 
@@ -57,35 +57,35 @@ app.UseAuthorization();
 
 var AgentURL = app.Configuration.GetValue<Uri>("AgentURL");
 
-app.UseProxies(proxies =>
-{
-    proxies.Map("/proxy/{query}", proxy => proxy.UseHttp(async (context, args) =>
-    {
-        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+// app.UseProxies(proxies =>
+// {
+//     proxies.Map("/proxy/{query}", proxy => proxy.UseHttp(async (context, args) =>
+//     {
+//         var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
 
-        logger.LogInformation($"Method: {context.Request.Method}, URL: {context.Request.GetDisplayUrl()}");
-        foreach (var header in context.Request.Headers)
-        {
-            logger.LogInformation($"Header: {header.Key}, Value: {header.Value}");
-        }
+//         logger.LogInformation($"Method: {context.Request.Method}, URL: {context.Request.GetDisplayUrl()}");
+//         foreach (var header in context.Request.Headers)
+//         {
+//             logger.LogInformation($"Header: {header.Key}, Value: {header.Value}");
+//         }
 
-        if (context.Request.ContentLength > 0 && (context.Request.Method == HttpMethods.Post || context.Request.Method == HttpMethods.Put))
-        {
-            context.Request.EnableBuffering();
-            var bodyReader = new StreamReader(context.Request.Body);
-            var bodyContent = await bodyReader.ReadToEndAsync();
-            context.Request.Body.Position = 0;
-            logger.LogInformation($"Body: {bodyContent}");
-        }
+//         if (context.Request.ContentLength > 0 && (context.Request.Method == HttpMethods.Post || context.Request.Method == HttpMethods.Put))
+//         {
+//             context.Request.EnableBuffering();
+//             var bodyReader = new StreamReader(context.Request.Body);
+//             var bodyContent = await bodyReader.ReadToEndAsync();
+//             context.Request.Body.Position = 0;
+//             logger.LogInformation($"Body: {bodyContent}");
+//         }
 
-        foreach (var arg in args)
-        {
-            logger.LogInformation($"Key: {arg.Key}, Value: {arg.Value}");
-        }
-        string? query = args["query"].ToString();
-        return $"{AgentURL}{query}";
-    }));
-});
+//         foreach (var arg in args)
+//         {
+//             logger.LogInformation($"Key: {arg.Key}, Value: {arg.Value}");
+//         }
+//         string? query = args["query"].ToString();
+//         return $"{AgentURL}{query}";
+//     }));
+// });
 
 app.UseCors(policy => policy
     .AllowAnyOrigin()
