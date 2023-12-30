cfccc8e99ec7babe216a30cac7d758b05fb90d6d
Guillaume <guillaume.terriere@epitech.eu>
2023-12-29 19:53:19 -0500

    Add HiveMiddleware to ApiGateway

---
+++ ApiGateway/HiveMiddleware/HiveMiddleware.cs
@@ -0,0 +1,98 @@
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace ApiGateway.HiveMiddleware
+{
+    public class HiveMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly HttpClient _httpClient;
+        private readonly Uri? _agentURL;
+
+        public HiveMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true // NOT IN PRODUCTION
+            };
+            _httpClient = new HttpClient(handler);
+            var url = configuration.GetValue<string>("AgentURL");
+            if (!string.IsNullOrEmpty(url))
+            {
+                _agentURL = new Uri(url);
+            } else {
+                _agentURL = null;
+            }
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var requestMethod = context.Request.Method;
+            var requestPath = context.Request.Path;
+
+            LogHive(context);
+
+            if (requestPath.StartsWithSegments("/proxy") && _agentURL != null)
+            {
+                var targetUri = new Uri(_agentURL, requestPath.Value.Substring(6));
+
+                var requestMessage = new HttpRequestMessage();
+
+                if (!HttpMethods.IsGet(requestMethod) && !HttpMethods.IsHead(requestMethod))
+                {
+                    var streamContent = new StreamContent(context.Request.Body);
+                    requestMessage.Content = streamContent;
+                }
+
+                foreach (var header in context.Request.Headers)
+                {
+                    requestMessage.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+                }
+
+                requestMessage.RequestUri = targetUri;
+                requestMessage.Method = new HttpMethod(requestMethod);
+
+                var responseMessage = await _httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted);
+
+                context.Response.StatusCode = (int)responseMessage.StatusCode;
+                foreach (var header in responseMessage.Headers)
+                {
+                    context.Response.Headers[header.Key] = header.Value.ToArray();
+                }
+
+                foreach (var header in responseMessage.Content.Headers)
+                {
+                    context.Response.Headers[header.Key] = header.Value.ToArray();
+                }
+
+                context.Response.Headers.Remove("transfer-encoding");
+
+                await responseMessage.Content.CopyToAsync(context.Response.Body);
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+
+        public static void LogHive(HttpContext context)
+        {
+            var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+
+            logger.LogInformation($"Method: {context.Request.Method}, URL: {context.Request.GetDisplayUrl()}");
+            foreach (var header in context.Request.Headers)
+            {
+                logger.LogInformation($"Header: {header.Key}, Value: {header.Value}");
+            }
+
+            if (context.Request.ContentLength > 0 && (context.Request.Method == HttpMethods.Post || context.Request.Method == HttpMethods.Put))
+            {
+                context.Request.EnableBuffering();
+                var bodyReader = new StreamReader(context.Request.Body);
+                var bodyContent = bodyReader.ReadToEndAsync().Result;
+                context.Request.Body.Position = 0;
+                logger.LogInformation($"Body: {bodyContent}");
+            }
+        }
+    }
+}
--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -27,38 +27,39 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 // app.UseHttpsRedirection();
+app.UseMiddleware<ApiGateway.HiveMiddleware.HiveMiddleware>();
 
-var AgentURL = app.Configuration.GetValue<Uri>("AgentURL");
+// var AgentURL = app.Configuration.GetValue<Uri>("AgentURL");
 
-app.UseProxies(proxies =>
-{
-    proxies.Map("/proxy/{query}", proxy => proxy.UseHttp(async (context, args) =>
-    {
-        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+// app.UseProxies(proxies =>
+// {
+//     proxies.Map("/proxy/{query}", proxy => proxy.UseHttp(async (context, args) =>
+//     {
+//         var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
 
-        logger.LogInformation($"Method: {context.Request.Method}, URL: {context.Request.GetDisplayUrl()}");
-        foreach (var header in context.Request.Headers)
-        {
-            logger.LogInformation($"Header: {header.Key}, Value: {header.Value}");
-        }
+//         logger.LogInformation($"Method: {context.Request.Method}, URL: {context.Request.GetDisplayUrl()}");
+//         foreach (var header in context.Request.Headers)
+//         {
+//             logger.LogInformation($"Header: {header.Key}, Value: {header.Value}");
+//         }
 
-        if (context.Request.ContentLength > 0 && (context.Request.Method == HttpMethods.Post || context.Request.Method == HttpMethods.Put))
-        {
-            context.Request.EnableBuffering();
-            var bodyReader = new StreamReader(context.Request.Body);
-            var bodyContent = await bodyReader.ReadToEndAsync();
-            context.Request.Body.Position = 0;
-            logger.LogInformation($"Body: {bodyContent}");
-        }
+//         if (context.Request.ContentLength > 0 && (context.Request.Method == HttpMethods.Post || context.Request.Method == HttpMethods.Put))
+//         {
+//             context.Request.EnableBuffering();
+//             var bodyReader = new StreamReader(context.Request.Body);
+//             var bodyContent = await bodyReader.ReadToEndAsync();
+//             context.Request.Body.Position = 0;
+//             logger.LogInformation($"Body: {bodyContent}");
+//         }
 
-        foreach (var arg in args)
-        {
-            logger.LogInformation($"Key: {arg.Key}, Value: {arg.Value}");
-        }
-        string? query = args["query"].ToString();
-        return $"{AgentURL}{query}";
-    }));
-});
+//         foreach (var arg in args)
+//         {
+//             logger.LogInformation($"Key: {arg.Key}, Value: {arg.Value}");
+//         }
+//         string? query = args["query"].ToString();
+//         return $"{AgentURL}{query}";
+//     }));
+// });
 
 app.UseAuthorization();
 
