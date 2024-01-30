5f738925b22d6ffa059f70c7b29010a1ce47aa7b
Tesnier Lucas <72015360+LucasTesnier@users.noreply.github.com>
2024-01-30 20:11:36 +0100

    Fix multi service architecture (#30)
    * Fix multi service architecture
    
    * fix ci

--- ApiGateway/Controllers/GatewayController.cs
+++ ApiGateway/Controllers/GatewayController.cs
@@ -13,11 +13,43 @@ namespace ApiGateway.Controllers
             _clientFactory = clientFactory;
         }
 
+        [HttpGet("auth/{*path}")]
+        public async Task<IActionResult> Auth(string path)
+        {
+            var client = _clientFactory.CreateClient("AuthServiceClient");
+            var requestPath = $"/{path}";
+            var response = await client.GetAsync(requestPath);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return Ok(responseContent);
+            }
+
+            return StatusCode((int)response.StatusCode, "Error calling Auth service");
+        }
+
         [HttpGet("auth/Agent/test")]
         public async Task<IActionResult> AuthAgentTest()
         {
             var client = _clientFactory.CreateClient("AuthServiceClient");
             var response = await client.GetAsync("/Agent/test");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return Ok(responseContent);
+            }
+
+            return StatusCode((int)response.StatusCode, "Error calling Auth service");
+        }
+
+        [HttpGet("dataProcessing/{*path}")]
+        public async Task<IActionResult> DataProcessing(string path)
+        {
+            var client = _clientFactory.CreateClient("DataProcessingServiceClient");
+            var requestPath = $"/{path}";
+            var response = await client.GetAsync(requestPath);
 
             if (response.IsSuccessStatusCode)
             {
