2a04a048b8ab5a057b3ca0b57a854c4b20de4199
Lucas Tesnier <lucas.tesnier@epitech.eu>
2024-01-30 20:42:13 +0100

    add put post delete method for gateway service

--- ApiGateway/Controllers/GatewayController.cs
+++ ApiGateway/Controllers/GatewayController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ApiGateway.Controllers
 {
@@ -14,11 +15,48 @@ namespace ApiGateway.Controllers
         }
 
         [HttpGet("auth/{*path}")]
-        public async Task<IActionResult> Auth(string path)
+        public async Task<IActionResult> AuthGet(string path)
+        {
+            return await SendAuthRequest(HttpMethod.Get, path, null);
+        }
+
+        [HttpPost("auth/{*path}")]
+        public async Task<IActionResult> AuthPost(string path, [FromBody] object requestBody)
+        {
+            return await SendAuthRequest(HttpMethod.Post, path, requestBody);
+        }
+
+        [HttpPut("auth/{*path}")]
+        public async Task<IActionResult> AuthPut(string path, [FromBody] object requestBody)
+        {
+            return await SendAuthRequest(HttpMethod.Put, path, requestBody);
+        }
+
+        [HttpDelete("auth/{*path}")]
+        public async Task<IActionResult> AuthDelete(string path)
+        {
+            return await SendAuthRequest(HttpMethod.Delete, path, null);
+        }
+
+        private async Task<IActionResult> SendAuthRequest(HttpMethod httpMethod, string path, object? requestBody)
         {
             var client = _clientFactory.CreateClient("AuthServiceClient");
             var requestPath = $"/{path}";
-            var response = await client.GetAsync(requestPath);
+
+            HttpRequestMessage request;
+
+            if (httpMethod == HttpMethod.Post || httpMethod == HttpMethod.Put)
+            {
+                var jsonRequestBody = JsonConvert.SerializeObject(requestBody);
+                var content = new StringContent(jsonRequestBody, System.Text.Encoding.UTF8, System.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/json"));
+                request = new HttpRequestMessage(httpMethod, requestPath) { Content = content };
+            }
+            else
+            {
+                request = new HttpRequestMessage(httpMethod, requestPath);
+            }
+
+            var response = await client.SendAsync(request);
 
             if (response.IsSuccessStatusCode)
             {
@@ -45,11 +83,48 @@ namespace ApiGateway.Controllers
         }
 
         [HttpGet("dataProcessing/{*path}")]
-        public async Task<IActionResult> DataProcessing(string path)
+        public async Task<IActionResult> DataProcessingGet(string path)
+        {
+            return await SendDataProcessingRequest(HttpMethod.Get, path, null);
+        }
+
+        [HttpPost("dataProcessing/{*path}")]
+        public async Task<IActionResult> DataProcessingPost(string path, [FromBody] object requestBody)
+        {
+            return await SendDataProcessingRequest(HttpMethod.Post, path, requestBody);
+        }
+
+        [HttpPut("dataProcessing/{*path}")]
+        public async Task<IActionResult> DataProcessingPut(string path, [FromBody] object requestBody)
+        {
+            return await SendDataProcessingRequest(HttpMethod.Put, path, requestBody);
+        }
+
+        [HttpDelete("dataProcessing/{*path}")]
+        public async Task<IActionResult> DataProcessingDelete(string path)
+        {
+            return await SendDataProcessingRequest(HttpMethod.Delete, path, null);
+        }
+
+        private async Task<IActionResult> SendDataProcessingRequest(HttpMethod httpMethod, string path, object? requestBody)
         {
             var client = _clientFactory.CreateClient("DataProcessingServiceClient");
             var requestPath = $"/{path}";
-            var response = await client.GetAsync(requestPath);
+
+            HttpRequestMessage request;
+
+            if (httpMethod == HttpMethod.Post || httpMethod == HttpMethod.Put)
+            {
+                var jsonRequestBody = JsonConvert.SerializeObject(requestBody);
+                var content = new StringContent(jsonRequestBody, System.Text.Encoding.UTF8, System.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/json"));
+                request = new HttpRequestMessage(httpMethod, requestPath) { Content = content };
+            }
+            else
+            {
+                request = new HttpRequestMessage(httpMethod, requestPath);
+            }
+
+            var response = await client.SendAsync(request);
 
             if (response.IsSuccessStatusCode)
             {
@@ -57,7 +132,7 @@ namespace ApiGateway.Controllers
                 return Ok(responseContent);
             }
 
-            return StatusCode((int)response.StatusCode, "Error calling Auth service");
+            return StatusCode((int)response.StatusCode, "Error calling Data Processing service");
         }
 
         [HttpGet("other")]
