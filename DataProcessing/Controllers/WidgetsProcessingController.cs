918b1a6a173ff1cea0633de3c6db0a2ec0806519
LucasTesnier <lucas.tesnier@epitech.eu>
2024-04-12 11:14:46 -0700

    test output dataprocess

--- DataProcessing/Controllers/WidgetsProcessingController.cs
+++ DataProcessing/Controllers/WidgetsProcessingController.cs
@@ -83,24 +83,6 @@ namespace WidgetController.Controllers
             return Ok(jsonData);
         }
 
-        [HttpGet("getTextWidgetheavy")]
-        public IActionResult GetTextWidgetheavy()
-        {
-            var data = new
-            {
-                title = "heavy",
-                types = "Number",
-                data = new
-                {
-                    percentage = "-5",
-                    value = "86",
-                    status = true
-                }
-            };
-            var jsonData = JsonConvert.SerializeObject(data);
-            return Ok(jsonData);
-        }
-
         [HttpGet("getTextWidgetstorage")]
         public IActionResult GetTextWidgetstorage()
         {
@@ -119,5 +101,109 @@ namespace WidgetController.Controllers
             var jsonData = JsonConvert.SerializeObject(data);
             return Ok(jsonData);
         }
+
+        [HttpGet("getOverviewAll")]
+        public IActionResult GetOverviewAll()
+        {
+            var data =
+            [{
+                pretty_path: "src/my_files.rs",
+                size: 21782,
+                last_modified:
+                {
+                    secs_since_epoch: 1706651511,
+                    nanos_since_epoch: 396799014
+                },
+                tidy_score:
+                {
+                    grade: 'B',
+                        misnamed:
+                    {
+                    grade: 'A',
+                        },
+                    unused:
+                    {
+                    grade: 'A'
+                        },
+                    duplicated:
+                    {
+                    grade: 'B'
+                        }
+                }
+            }];
+
+            var jsonData = JsonConvert.SerializeObject(data);
+            return Ok(jsonData);
+        }
+
+        [HttpGet("getOverviewMisnamed")]
+        public IActionResult GetOverviewMisnamed()
+        {
+            var data =
+            [{
+                pretty_path: "src/my_files.rs",
+                size: 21782,
+                last_modified:
+                {
+                    secs_since_epoch: 1706651511,
+                    nanos_since_epoch: 396799014
+                },
+                tidy_regle:
+                {
+                    grade: 'A',
+                    configurations: [{name: 'date', grade: 'A', weight: 3}, {name: 'valid separator', grade: 'A', weight: 1.8}]
+                }
+            }];
+
+            var jsonData = JsonConvert.SerializeObject(data);
+            return Ok(jsonData);
+        }
+
+        [HttpGet("getOverviewDuplicate")]
+        public IActionResult GetOverviewDuplicate()
+        {
+            var data =
+            [{
+                pretty_path: "src/my_files.rs",
+                size: 21782,
+                last_modified:
+                {
+                    secs_since_epoch: 1706651511,
+                    nanos_since_epoch: 396799014
+                },
+                tidy_regle:
+                {
+                    grade: 'B',
+                    configurations: [{name: 'occurence', grade: 'B', weight: 1}]
+                }
+            }];
+
+            var jsonData = JsonConvert.SerializeObject(data);
+            return Ok(jsonData);
+        }
+
+        [HttpGet("getOverviewUnused")]
+        public IActionResult GetOverviewUnused()
+        {
+            var data =
+            [{
+                pretty_path: "src/my_files.rs",
+                size: 21782,
+                last_modified:
+                {
+                    secs_since_epoch: 1706651511,
+                    nanos_since_epoch: 396799014
+                },
+                tidy_regle:
+                {
+                    grade: 'C',
+                    configurations: [{name: 'perished', grade: 'C', weight: 1}]
+                }
+            }];
+
+            var jsonData = JsonConvert.SerializeObject(data);
+            return Ok(jsonData);
+        }
+
     }
 }
