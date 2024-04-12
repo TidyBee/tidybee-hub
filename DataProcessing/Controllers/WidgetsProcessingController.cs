c2d734d83ab0be4e6c514140a4e1a5d0a1b973a6
LucasTesnier <lucas.tesnier@epitech.eu>
2024-04-12 11:45:24 -0700

    fix typo issue

--- DataProcessing/Controllers/WidgetsProcessingController.cs
+++ DataProcessing/Controllers/WidgetsProcessingController.cs
@@ -105,137 +105,178 @@ namespace WidgetController.Controllers
         [HttpGet("getOverviewAll")]
         public IActionResult GetOverviewAll()
         {
-            var data = new[] {
+            var data = new[]
+            {
                 new {
-                pretty_path: "src/my_files.rs",
-                size: 21782,
-                last_modified:
-            new
-            {
-                secs_since_epoch: 1706651511,
-                nanos_since_epoch: 396799014
-            },
-                tidy_score:
-            new
-            {
-                grade: 'B',
-                misnamed:
-                    new
+                    pretty_path = "src/my_files.rs",
+                    size = 21782,
+                    last_modified = new
                     {
-                        grade: 'A',
+                        secs_since_epoch = 1706651511,
+                        nanos_since_epoch = 396799014
                     },
-                unused:
-                    new
+                    tidy_score = new
                     {
-                        grade: 'A'
-                    },
-                duplicated:
-                    new
-                    {
-                        grade: 'B'
+                        grade = 'B',
+                        misnamed = new
+                        {
+                            grade = 'A'
+                        },
+                        unused = new
+                        {
+                            grade = 'A'
+                        },
+                        duplicated = new
+                        {
+                            grade = 'B'
+                        }
                     }
-            }
-            }
-    };
-
-    var jsonData = JsonConvert.SerializeObject(data);
-            return Ok(jsonData);
-}
-
-[HttpGet("getOverviewMisnamed")]
-public IActionResult GetOverviewMisnamed()
-{
-    var data = new
-    [{
-                pretty_path: "src/my_files.rs",
-                size: 21782,
-                last_modified:
-{
-secs_since_epoch: 1706651511,
-                    nanos_since_epoch: 396799014
-                },
-                tidy_regle:
-{
-grade: 'A',
-                    configurations: [{ name: 'date', grade: 'A', weight: 3}, { name: 'valid separator', grade: 'A', weight: 1.8}]
                 }
-            }];
+            };
 
-var jsonData = JsonConvert.SerializeObject(data);
-return Ok(jsonData);
+            var jsonData = JsonConvert.SerializeObject(data);
+            return Ok(jsonData);
+        }
+
+        [HttpGet("getOverviewMisnamed")]
+        public IActionResult GetOverviewMisnamed()
+        {
+            var data = new[]
+            {
+                new {
+                    pretty_path = "src/my_files.rs",
+                    size = 21782,
+                    last_modified = new
+                    {
+                        secs_since_epoch = 1706651511,
+                        nanos_since_epoch = 396799014
+                    },
+                    tidy_regle = new
+                    {
+                        grade = 'A',
+                        configurations = new[]
+                        {
+                            new {
+                                name = "date",
+                                grade = 'A',
+                                weight = 3
+                            },
+                            new {
+                                name = "valid separator",
+                                grade = 'A',
+                                weight = 1.8
+                            }
+                        }
+                    }
+                }
+            };
+
+            var jsonData = JsonConvert.SerializeObject(data);
+            return Ok(jsonData);
         }
 
         [HttpGet("getOverviewDuplicate")]
-public IActionResult GetOverviewDuplicate()
-{
-    var data = new
-    [{
-                pretty_path: "src/my_files.rs",
-                size: 21782,
-                last_modified:
-{
-secs_since_epoch: 1706651511,
-                    nanos_since_epoch: 396799014
-                },
-                tidy_regle:
-{
-grade: 'B',
-                    configurations: [{ name: 'occurence', grade: 'B', weight: 1}]
+        public IActionResult GetOverviewDuplicate()
+        {
+            var data = new[]
+            {
+                new {
+                    pretty_path = "src/my_files.rs",
+                    size = 21782,
+                    last_modified = new
+                    {
+                        secs_since_epoch = 1706651511,
+                        nanos_since_epoch = 396799014
+                    },
+                    tidy_regle = new
+                    {
+                        grade = 'B',
+                        configurations = new[]
+                        {
+                            new {
+                                name = "occurence",
+                                grade = 'B',
+                                weight = 1
+                            }
+                        }
+                    }
                 }
-            }];
+            };
 
-var jsonData = JsonConvert.SerializeObject(data);
-return Ok(jsonData);
+            var jsonData = JsonConvert.SerializeObject(data);
+            return Ok(jsonData);
         }
+
 
         [HttpGet("getOverviewUnused")]
-public IActionResult GetOverviewUnused()
-{
-    var data = new
-    [{
-                pretty_path: "src/my_files.rs",
-                size: 21782,
-                last_modified:
-{
-secs_since_epoch: 1706651511,
-                    nanos_since_epoch: 396799014
-                },
-                tidy_regle:
-{
-grade: 'C',
-                    configurations: [{ name: 'perished', grade: 'C', weight: 1}]
+        public IActionResult GetOverviewUnused()
+        {
+            var data = new[]
+            {
+                new {
+                    pretty_path = "src/my_files.rs",
+                    size = 21782,
+                    last_modified = new
+                    {
+                        secs_since_epoch = 1706651511,
+                        nanos_since_epoch = 396799014
+                    },
+                    tidy_regle = new
+                    {
+                        grade = 'C',
+                        configurations = new[]
+                        {
+                            new {
+                                name = "perished",
+                                grade = 'C',
+                                weight = 1
+                            }
+                        }
+                    }
                 }
-            }];
+            };
 
-var jsonData = JsonConvert.SerializeObject(data);
-return Ok(jsonData);
+            var jsonData = JsonConvert.SerializeObject(data);
+            return Ok(jsonData);
         }
+
 
         [HttpGet("getTidyRules")]
-public IActionResult GetTidyRules()
-{
-    var data = new
-    [{
-                rules:
-                [
+        public IActionResult GetTidyRules()
+        {
+            var data = new[]
+            {
+                new {
+                    rules = new[]
                     {
-                        name: 'misnamed',
-                        configurations: [{name: 'date', weight: 3, description: 'The name of the file need to have a date', regex: "r'_\d{4}\.'"}, { name: 'valid separator', weight: 1.8, description: 'The name of the file need to have 4 separators _', regex: "r'^[^_]*(_[^_]*){3}$'"}]
-                    },
-                    {
-name: 'duplicate',
-                        configurations: [{ name: 'occurence', weight: 1, description: 'The file need to be unique', limitInt: 1}]
-                    },
-                    {
-name: 'unused',
-                        configurations: [{ name: 'perished', weight: 1, description: 'The file need to be recent enough', limitISO: '2024-04-12T00:00:00Z'}]
+                        new {
+                            name = "misnamed",
+                            configurations = new[]
+                            {
+                                new { name = "date", weight = 3, description = "The name of the file need to have a date", regex = @"r'_\d{4}\.'" },
+                                new { name = "valid separator", weight = 1.8, description = "The name of the file need to have 4 separators _", regex = @"r'^[^_]*(_[^_]*){3}$'" }
+                            }
+                        },
+                        new {
+                            name = "duplicate",
+                            configurations = new[]
+                            {
+                                new { name = "occurence", weight = 1, description = "The file need to be unique", limitInt = 1 }
+                            }
+                        },
+                        new {
+                            name = "unused",
+                            configurations = new[]
+                            {
+                                new { name = "perished", weight = 1, description = "The file need to be recent enough", limitISO = "2024-04-12T00:00:00Z" }
+                            }
+                        }
                     }
-                ]
-            }];
+                }
+            };
 
-var jsonData = JsonConvert.SerializeObject(data);
-return Ok(jsonData);
+            var jsonData = JsonConvert.SerializeObject(data);
+            return Ok(jsonData);
         }
-
     }
 }
