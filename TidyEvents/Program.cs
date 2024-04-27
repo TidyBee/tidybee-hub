f2c4ee7971d2c6b23c41c6faef06ba1d2a438a5c
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2024-04-27 11:38:20 +0100

    feat: Add TidyEvents project to solution
    Added TidyEvents project to the solution with necessary configurations,
    including target framework, package references, and protobuf definition
    files. This project will handle file events received from the Agent.

---
+++ TidyEvents/Program.cs
@@ -0,0 +1,21 @@
+using TestgRPC.Interceptors;
+using TestgRPC.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc().AddServiceOptions<TidyBeeEventsService>(options =>
+{
+    options.Interceptors.Add<AuthInterceptor>();
+});
+
+builder.Services.Configure<AuthInterceptorOption>(builder.Configuration.GetSection("AuthInterceptor"));
+builder.Services.AddHttpClient<AuthInterceptor>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<TidyBeeEventsService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
