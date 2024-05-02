1c88fea5159d3b18f620d9f0f8aa2999faf92e0e
LucasTesnier <lucas.tesnier@epitech.eu>
2024-05-01 19:51:49 -0700

    add output service

--- DataProcessing/Services/InputService.cs
+++ DataProcessing/Services/InputService.cs
@@ -62,7 +62,7 @@ public class InputService
                                     }
                                 }
                             }
-                        }
+                        };
         return rules;
     }
 
