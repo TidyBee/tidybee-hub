df6fb8cd8f4f3b9d43b2c8f290ea0402b5a3eb7a
LucasTesnier <lucas.tesnier@epitech.eu>
2024-05-01 19:13:57 -0700

    add output service

--- DataProcessing/Controllers/WidgetsProcessingController.cs
+++ DataProcessing/Controllers/WidgetsProcessingController.cs
@@ -18,327 +18,74 @@ namespace WidgetController.Controllers
         [HttpGet("getTextWidgetunused")]
         public IActionResult GetTextWidgetunused()
         {
-
-            return Ok(_outputService.GetTextWidgetUnused());
+            return Ok(_outputService.getTextWidgetUnused());
         }
 
         [HttpGet("getGradeWidget")]
         public IActionResult getGradeWidget()
         {
-            var data = new
-            {
-                grade = "B"
-            };
-            var jsonData = JsonConvert.SerializeObject(data);
-            return Ok(jsonData);
+            return Ok(_outputService.getGradeWidget());
         }
 
         [HttpGet("getTotalMonitored")]
         public IActionResult getTotalMonitored()
         {
-            var data = new {
-                title = "total",
-                types = "Number",
-                data = new
-                {
-                    percentage = "+2",
-                    value = "105",
-                    status = true
-                }
-            };
-            var jsonData = JsonConvert.SerializeObject(data);
-            return Ok(jsonData);
+            return Ok(_outputService.getTotalMonitored());
         }
 
         [HttpGet("getGraphWidget")]
         public IActionResult getGraphWidget()
         {
-            var data = new
-            {
-                series = new[] { 20, 32, 23, 15, 10 }
-            };
-            var jsonData = JsonConvert.SerializeObject(data);
-            return Ok(jsonData);
+            return Ok(_outputService.getGraphWidget);
         }
 
         [HttpGet("getTextWidgetbadname")]
         public IActionResult GetTextWidgetbadname()
         {
-            var data = new
-            {
-                title = "badname",
-                types = "Number",
-                data = new
-                {
-                    percentage = "-12",
-                    value = "259",
-                    status = true
-                }
-            };
-            var jsonData = JsonConvert.SerializeObject(data);
-            return Ok(jsonData);
+            return Ok(_outputService.getTextWidgetbadname());
         }
 
         [HttpGet("getTextWidgetduplicate")]
         public IActionResult GetTextWidgetduplicate()
         {
-            var data = new
-            {
-                title = "duplicate",
-                types = "Number",
-                data = new
-                {
-                    percentage = "+19",
-                    value = "124",
-                    status = false
-                }
-            };
-            var jsonData = JsonConvert.SerializeObject(data);
-            return Ok(jsonData);
+            return Ok(_outputService.getTextWidgetduplicate());
         }
 
         [HttpGet("getTextWidgetstorage")]
         public IActionResult GetTextWidgetstorage()
         {
-            var data = new
-            {
-                title = "storage",
-                types = "Graph",
-                data = new
-                {
-                    percentage = "+4",
-                    value = "237/512GB",
-                    valuePercentage = "28.32",
-                    status = false
-                }
-            };
-            var jsonData = JsonConvert.SerializeObject(data);
-            return Ok(jsonData);
+            return Ok(_outputService.getTextWidgetstorage());
         }
 
 
         [HttpGet("getOverviewAll")]
         public IActionResult GetOverviewAll()
         {
-            var data = new[]
-            {
-                new Overview
-                {
-                    pretty_path = "src/my_files.rs",
-                    size = 21782,
-                    last_modified = new LastModified
-                    {
-                        secs_since_epoch = 1706651511,
-                        nanos_since_epoch = 396799014
-                    },
-                    tidy_score = new TidyScore
-                    {
-                        grade = 'B',
-                        misnamed = new Misnamed
-                        {
-                            grade = 'A'
-                        },
-                        unused = new Unused
-                        {
-                            grade = 'A'
-                        },
-                        duplicated = new Duplicated
-                        {
-                            grade = 'B'
-                        }
-                    }
-                }
-            };
-
-            var jsonData = JsonConvert.SerializeObject(data);
-            return Ok(jsonData);
+            return Ok(_outputService.getOverviewAll());
         }
 
         [HttpGet("getOverviewMisnamed")]
         public IActionResult GetOverviewMisnamed()
         {
-            var data = new[]
-            {
-                new Overview
-                {
-                    pretty_path = "src/my_files.rs",
-                    size = 21782,
-                    last_modified = new LastModified
-                    {
-                        secs_since_epoch = 1706651511,
-                        nanos_since_epoch = 396799014
-                    },
-                    tidy_score = new TidyScore
-                    {
-                        grade = 'A',
-                        misnamed = new Misnamed
-                        {
-                            grade = 'A',
-                            configurations = new List<Configuration>
-                            {
-                                new Configuration
-                                {
-                                    name = "date",
-                                    grade = 'A',
-                                    weight = 3
-                                },
-                                new Configuration
-                                {
-                                    name = "valid separator",
-                                    grade = 'A',
-                                    weight = 1.8
-                                }
-                            }
-                        }
-                    }
-                }
-            };
-
-            var jsonData = JsonConvert.SerializeObject(data);
-            return Ok(jsonData);
+            return Ok(_outputService.getOverviewMisnamed());
         }
 
         [HttpGet("getOverviewDuplicate")]
         public IActionResult GetOverviewDuplicate()
         {
-            var data = new[]
-            {
-                new Overview
-                {
-                    pretty_path = "src/my_files.rs",
-                    size = 21782,
-                    last_modified = new LastModified
-                    {
-                        secs_since_epoch = 1706651511,
-                        nanos_since_epoch = 396799014
-                    },
-                    tidy_score = new TidyScore
-                    {
-                        grade = 'B',
-                        duplicated = new Duplicated
-                        {
-                            grade = 'B',
-                            configurations = new List<Configuration>
-                            {
-                                new Configuration
-                                {
-                                    name = "occurrence",
-                                    grade = 'B',
-                                    weight = 1,
-                                    description = "The file need to be unique",
-                                    limitInt = 1
-                                }
-                            }
-                        }
-                    }
-                }
-            };
-
-            var jsonData = JsonConvert.SerializeObject(data);
-            return Ok(jsonData);
+            return Ok(_outputService.getOverviewDuplicate());
         }
 
         [HttpGet("getOverviewUnused")]
         public IActionResult GetOverviewUnused()
         {
-            var data = new[]
-            {
-                new Overview
-                {
-                    pretty_path = "src/my_files.rs",
-                    size = 21782,
-                    last_modified = new LastModified
-                    {
-                        secs_since_epoch = 1706651511,
-                        nanos_since_epoch = 396799014
-                    },
-                    tidy_score = new TidyScore
-                    {
-                        grade = 'C',
-                        unused = new Unused
-                        {
-                            grade = 'C',
-                            configurations = new List<Configuration>
-                            {
-                                new Configuration
-                                {
-                                    name = "perished",
-                                    grade = 'C',
-                                    weight = 1,
-                                    description = "The file need to be recent enough",
-                                    limitISO = "2024-04-12T00:00:00Z"
-                                }
-                            }
-                        }
-                    }
-                }
-            };
-
-            var jsonData = JsonConvert.SerializeObject(data);
-            return Ok(jsonData);
+            return Ok(_outputService.getOverviewUnused());
         }
 
         [HttpGet("getTidyRules")]
         public IActionResult GetTidyRules()
         {
-            var data = new TidyRule
-            {
-                rules = new List<Rule>
-                {
-                    new Rule
-                    {
-                        name = "misnamed",
-                        configurations = new List<Configuration>
-                        {
-                            new Configuration
-                            {
-                                name = "date",
-                                weight = 3,
-                                description = "The name of the file need to have a date",
-                                regex = @"r'_\d{4}\.'"
-                            },
-                            new Configuration
-                            {
-                                name = "valid separator",
-                                weight = 1.8,
-                                description = "The name of the file need to have 4 separators _",
-                                regex = @"r'^[^_]*(_[^_]*){3}$'"
-                            }
-                        }
-                    },
-                    new Rule
-                    {
-                        name = "duplicate",
-                        configurations = new List<Configuration>
-                        {
-                            new Configuration
-                            {
-                                name = "occurrence",
-                                weight = 1,
-                                description = "The file need to be unique",
-                                limitInt = 1
-                            }
-                        }
-                    },
-                    new Rule
-                    {
-                        name = "unused",
-                        configurations = new List<Configuration>
-                        {
-                            new Configuration
-                            {
-                                name = "perished",
-                                weight = 1,
-                                description = "The file need to be recent enough",
-                                limitISO = "2024-04-12T00:00:00Z"
-                            }
-                        }
-                    }
-                }
-            };
-
-            var jsonData = JsonConvert.SerializeObject(data);
-            return Ok(jsonData);
+            return Ok(_outputService.getTidyRules());
         }
     }
 }
--- DataProcessing/Services/OutputService.cs
+++ DataProcessing/Services/OutputService.cs
@@ -9,7 +9,7 @@ public class OutputService
 
     }
 
-    public string GetTextWidgetUnused()
+    public string getTextWidgetUnused()
     {
         var data = new
         {
@@ -25,4 +25,314 @@ public class OutputService
         var jsonData = JsonConvert.SerializeObject(data);
         return jsonData;
     }
+
+    public string getGradeWidget()
+    {
+        var data = new
+        {
+            grade = "B"
+        };
+        var jsonData = JsonConvert.SerializeObject(data);
+        return jsonData;
+    }
+
+    public string getTotalMonitored()
+    {
+        var data = new
+        {
+            title = "total",
+            types = "Number",
+            data = new
+            {
+                percentage = "+2",
+                value = "105",
+                status = true
+            }
+        };
+        var jsonData = JsonConvert.SerializeObject(data);
+        return jsonData;
+    }
+
+    public string getGraphWidget()
+    {
+        var data = new
+        {
+            series = new[] { 20, 32, 23, 15, 10 }
+        };
+        var jsonData = JsonConvert.SerializeObject(data);
+        return jsonData;
+    }
+
+    public string getTextWidgetbadname()
+    {
+        var data = new
+        {
+            title = "badname",
+            types = "Number",
+            data = new
+            {
+                percentage = "-12",
+                value = "259",
+                status = true
+            }
+        };
+        var jsonData = JsonConvert.SerializeObject(data);
+        return jsonData;
+    }
+
+    public string getTextWidgetduplicate()
+    {
+        var data = new
+        {
+            title = "duplicate",
+            types = "Number",
+            data = new
+            {
+                percentage = "+19",
+                value = "124",
+                status = false
+            }
+        };
+        var jsonData = JsonConvert.SerializeObject(data);
+        return jsonData;
+    }
+
+    public string getTextWidgetstorage()
+    {
+        var data = new
+        {
+            title = "storage",
+            types = "Graph",
+            data = new
+            {
+                percentage = "+4",
+                value = "237/512GB",
+                valuePercentage = "28.32",
+                status = false
+            }
+        };
+        var jsonData = JsonConvert.SerializeObject(data);
+        return jsonData;
+    }
+
+    public string getOverviewAll()
+    {
+        var data = new[]
+        {
+                new Overview
+                {
+                    pretty_path = "src/my_files.rs",
+                    size = 21782,
+                    last_modified = new LastModified
+                    {
+                        secs_since_epoch = 1706651511,
+                        nanos_since_epoch = 396799014
+                    },
+                    tidy_score = new TidyScore
+                    {
+                        grade = 'B',
+                        misnamed = new Misnamed
+                        {
+                            grade = 'A'
+                        },
+                        unused = new Unused
+                        {
+                            grade = 'A'
+                        },
+                        duplicated = new Duplicated
+                        {
+                            grade = 'B'
+                        }
+                    }
+                }
+            };
+
+        var jsonData = JsonConvert.SerializeObject(data);
+        return jsonData;
+    }
+
+    public string getOverviewMisnamed()
+    {
+        var data = new[]
+        {
+                new Overview
+                {
+                    pretty_path = "src/my_files.rs",
+                    size = 21782,
+                    last_modified = new LastModified
+                    {
+                        secs_since_epoch = 1706651511,
+                        nanos_since_epoch = 396799014
+                    },
+                    tidy_score = new TidyScore
+                    {
+                        grade = 'A',
+                        misnamed = new Misnamed
+                        {
+                            grade = 'A',
+                            configurations = new List<Configuration>
+                            {
+                                new Configuration
+                                {
+                                    name = "date",
+                                    grade = 'A',
+                                    weight = 3
+                                },
+                                new Configuration
+                                {
+                                    name = "valid separator",
+                                    grade = 'A',
+                                    weight = 1.8
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+        var jsonData = JsonConvert.SerializeObject(data);
+        return jsonData;
+    }
+
+    public string getOverviewDuplicate()
+    {
+        var data = new[]
+        {
+                new Overview
+                {
+                    pretty_path = "src/my_files.rs",
+                    size = 21782,
+                    last_modified = new LastModified
+                    {
+                        secs_since_epoch = 1706651511,
+                        nanos_since_epoch = 396799014
+                    },
+                    tidy_score = new TidyScore
+                    {
+                        grade = 'B',
+                        duplicated = new Duplicated
+                        {
+                            grade = 'B',
+                            configurations = new List<Configuration>
+                            {
+                                new Configuration
+                                {
+                                    name = "occurrence",
+                                    grade = 'B',
+                                    weight = 1,
+                                    description = "The file need to be unique",
+                                    limitInt = 1
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+        var jsonData = JsonConvert.SerializeObject(data);
+        return jsonData;
+    }
+
+    public string getOverviewUnused()
+    {
+        var data = new[]
+        {
+                new Overview
+                {
+                    pretty_path = "src/my_files.rs",
+                    size = 21782,
+                    last_modified = new LastModified
+                    {
+                        secs_since_epoch = 1706651511,
+                        nanos_since_epoch = 396799014
+                    },
+                    tidy_score = new TidyScore
+                    {
+                        grade = 'C',
+                        unused = new Unused
+                        {
+                            grade = 'C',
+                            configurations = new List<Configuration>
+                            {
+                                new Configuration
+                                {
+                                    name = "perished",
+                                    grade = 'C',
+                                    weight = 1,
+                                    description = "The file need to be recent enough",
+                                    limitISO = "2024-04-12T00:00:00Z"
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+        var jsonData = JsonConvert.SerializeObject(data);
+        return jsonData;
+    }
+
+    public string getTidyRules()
+    {
+        var data = new TidyRule
+        {
+            rules = new List<Rule>
+                {
+                    new Rule
+                    {
+                        name = "misnamed",
+                        configurations = new List<Configuration>
+                        {
+                            new Configuration
+                            {
+                                name = "date",
+                                weight = 3,
+                                description = "The name of the file need to have a date",
+                                regex = @"r'_\d{4}\.'"
+                            },
+                            new Configuration
+                            {
+                                name = "valid separator",
+                                weight = 1.8,
+                                description = "The name of the file need to have 4 separators _",
+                                regex = @"r'^[^_]*(_[^_]*){3}$'"
+                            }
+                        }
+                    },
+                    new Rule
+                    {
+                        name = "duplicate",
+                        configurations = new List<Configuration>
+                        {
+                            new Configuration
+                            {
+                                name = "occurrence",
+                                weight = 1,
+                                description = "The file need to be unique",
+                                limitInt = 1
+                            }
+                        }
+                    },
+                    new Rule
+                    {
+                        name = "unused",
+                        configurations = new List<Configuration>
+                        {
+                            new Configuration
+                            {
+                                name = "perished",
+                                weight = 1,
+                                description = "The file need to be recent enough",
+                                limitISO = "2024-04-12T00:00:00Z"
+                            }
+                        }
+                    }
+                }
+        };
+
+        var jsonData = JsonConvert.SerializeObject(data);
+        return jsonData;
+    }
+
+
 }
