a161ecce7f8b01469967de6c2697c6aa3f5cb4ac
Blynqs <baptiste.friboulet@epitech.eu>
2024-05-23 21:34:41 +0900

    installer: set dataProcessing as windows service

--- DataProcessing/Program.cs
+++ DataProcessing/Program.cs
@@ -1,17 +1,24 @@
+using Microsoft.Extensions.Hosting.WindowsServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
+if (WindowsServiceHelpers.IsWindowsService())
+{
+    builder.Host.UseWindowsService();
+}
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var configPath = Path.Combine(AppContext.BaseDirectory, "appsettings.json");
+builder.Configuration.SetBasePath(AppContext.BaseDirectory)
+                      .AddJsonFile(configPath, optional: false, reloadOnChange: true);
+
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
