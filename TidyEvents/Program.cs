3d2cf6a840553e025b7e0a0126fa5357c8ee9d22
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2024-05-25 00:07:49 +0100

    TidyEvents: Manipulate files in postgres database

--- TidyEvents/Program.cs
+++ TidyEvents/Program.cs
@@ -1,18 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using TidyEvents.Context;
+using TidyEvents;
 using TidyEvents.Interceptors;
 using TidyEvents.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnection")));
 builder.Services.AddGrpc().AddServiceOptions<TidyBeeEventsService>(options =>
 {
     options.Interceptors.Add<AuthInterceptor>();
 });
-
 builder.Services.Configure<AuthInterceptorOption>(builder.Configuration.GetSection("AuthInterceptor"));
 builder.Services.AddHttpClient<AuthInterceptor>();
 
 var app = builder.Build();
+var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider.GetRequiredService<DatabaseContext>().Database.EnsureCreated();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<TidyBeeEventsService>();
--- TidyEvents/Services/TidyBeeEventsService.cs
+++ TidyEvents/Services/TidyBeeEventsService.cs
@@ -1,5 +1,7 @@
 using Grpc.Core;
-using Microsoft.AspNetCore.Authentication;
+using TidyEvents.Models;
+using TidyEvents.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace TidyEvents.Services;
 
@@ -7,16 +9,50 @@ public class TidyBeeEventsService : TidyBeeEvents.TidyBeeEventsBase
 {
     private readonly ILogger<TidyBeeEventsService> _logger;
 
-    public TidyBeeEventsService(ILogger<TidyBeeEventsService> logger)
+    private readonly DatabaseContext _context;
+
+    public TidyBeeEventsService(ILogger<TidyBeeEventsService> logger, DatabaseContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public override async Task<FileInfoEventResponse> FileEvent(IAsyncStreamReader<FileEventRequest> request, ServerCallContext context)
     {
+        // TODO: Buffer events and process them in batches
         await foreach (var update_request in request.ReadAllAsync())
         {
-            _logger.LogInformation($"Recieved a request to update file: {update_request}");
+            _logger.LogInformation($"Recieved a request type {update_request.EventType} to update file: {update_request.Path}");
+
+            if (update_request.EventType == FileEventType.Created)
+            {
+                await _context.Files.AddAsync(new Models.File
+                {
+                    Name = update_request.Path,
+                    FileHash = update_request.Hash,
+                    Size = (int)update_request.Size,
+                    LastModified = DateTime.Parse(update_request.LastModified.ToString().TrimStart('\"').TrimEnd('\"')),
+                    MisnamedScore = 'u',
+                    PerishedScore = 'u',
+                    DuplicatedScore = 'u',
+                    GlobalScore = 'u',
+                    DuplicateAssociativeTableDuplicateFiles = new List<DuplicateAssociativeTable>(),
+                    DuplicateAssociativeTableOriginalFiles = new List<DuplicateAssociativeTable>(),
+                });
+            }
+            if (update_request.EventType == FileEventType.Deleted)
+            {
+                var file = await _context.Files.Where(f => f.Name == update_request.Path).FirstOrDefaultAsync();
+                if (file != null)
+                {
+                    _context.Files.Remove(file);
+                } else {
+                    _logger.LogWarning($"File {update_request.Path} not found in database");
+                }
+            }
+
+            await _context.SaveChangesAsync();
+            _context.ChangeTracker.Clear();
         }
 
         return await Task.FromResult(new FileInfoEventResponse
