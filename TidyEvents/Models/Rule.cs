0867dcec1fc550fcf9ecd3957ef722418203e1bf
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2024-05-25 22:13:14 +0100

    Adding and Deleting file from the postgres database  (#53)
    * feat: Add TidyEvents project to solution
    
    Added TidyEvents project to the solution with necessary configurations,
    including target framework, package references, and protobuf definition
    files. This project will handle file events received from the Agent.
    
    * Remove nested project uuid from .sln
    
    * Add debug configurations for TidyEvents project
    
    * refacto: fix namespace
    
    * refacto: Fix HubUrl port
    
    * TidyEvents: Manipulate files in postgres database
    
    * TidyEvents: change 'u' to 'U' in default values of sores
    
    Co-authored-by: Julien Hourcadette <53608713+Bootoyka@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Julien Hourcadette <53608713+Bootoyka@users.noreply.github.com>

---
+++ TidyEvents/Models/Rule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TidyEvents.Models;
+
+public partial class Rule
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public double Weight { get; set; }
+
+    public string RulesConfig { get; set; } = null!;
+}
