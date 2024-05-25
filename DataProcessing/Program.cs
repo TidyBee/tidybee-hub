23d2a7c3c2098a7690b348eb8c97117a3e3c101a
LucasTesnier <lucas.tesnier@epitech.eu>
2024-05-26 00:13:41 +0200

    link DB to dataprocessing tidyRules

--- DataProcessing/Program.cs
+++ DataProcessing/Program.cs
@@ -11,8 +11,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<OutputService>();
 builder.Services.AddScoped<InputService>();
 
+Console.WriteLine(builder.Configuration.GetConnectionString("DatabaseConnection"));
 builder.Services.AddDbContext<DatabaseContext>(options =>
-    options.UseNpgsql("Host=hub-postgres;Database=db;Username=user;Password=pass"));
+    options.UseNpgsql("Host=hub-postgres;Database=db;Username=user;Password=pass");
 
 var app = builder.Build();
 var scope = app.Services.CreateScope();
