ef01cb7db084f636b2b952ffe86fb6dc52289434
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2024-05-26 12:47:18 +0100

    TidyEvents: call stored procedures after inserting in database

--- TidyEvents/Services/TidyBeeEventsService.cs
+++ TidyEvents/Services/TidyBeeEventsService.cs
@@ -1,7 +1,8 @@
 using Grpc.Core;
-using TidyEvents.Models;
 using TidyEvents.Context;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System.Runtime.CompilerServices;
 
 namespace TidyEvents.Services;
 
@@ -36,8 +37,8 @@ public class TidyBeeEventsService : TidyBeeEvents.TidyBeeEventsBase
                     PerishedScore = 'U',
                     DuplicatedScore = 'U',
                     GlobalScore = 'U',
-                    DuplicateAssociativeTableDuplicateFiles = new List<DuplicateAssociativeTable>(),
-                    DuplicateAssociativeTableOriginalFiles = new List<DuplicateAssociativeTable>(),
+                    DuplicateAssociativeTableDuplicateFiles = [],
+                    DuplicateAssociativeTableOriginalFiles = [],
                 });
             }
             if (update_request.EventType == FileEventType.Deleted)
@@ -46,13 +47,36 @@ public class TidyBeeEventsService : TidyBeeEvents.TidyBeeEventsBase
                 if (file != null)
                 {
                     _context.Files.Remove(file);
-                } else {
+                }
+                else
+                {
                     _logger.LogWarning($"File {update_request.Path} not found in database");
                 }
             }
 
             await _context.SaveChangesAsync();
             _context.ChangeTracker.Clear();
+        }
+
+        var stored_procedures_raw = new List<string>
+        {
+            "CALL calculate_every_perished_scores();",
+            "CALL calculate_every_misnamed_scores();",
+            "CALL calculate_every_duplicated_scores();",
+            "CALL calculate_every_global_scores();"
+        };
+        var stored_procedure_fs = stored_procedures_raw.Select(sp_raw => FormattableStringFactory.Create(sp_raw));
+
+        foreach (var sp in stored_procedure_fs)
+        {
+            try
+            {
+                await _context.Database.ExecuteSqlAsync(sp);
+            }
+            catch (NpgsqlException e)
+            {
+                _logger.LogError(e, $"Error executing stored procedure {sp}");
+            }
         }
 
         return await Task.FromResult(new FileInfoEventResponse
