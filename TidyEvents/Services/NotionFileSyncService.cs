e45a9e5bfd600ac142e0762ee31499035209ede5
LucasTesnier <lucas.tesnier@epitech.eu>
2024-08-20 21:28:07 +0200

    notion integration

--- TidyEvents/Services/NotionFileSyncService.cs
+++ TidyEvents/Services/NotionFileSyncService.cs
@@ -39,62 +39,8 @@ namespace TidyEvents.Services
                     var propertyName = property.Key;
                     var propertyValue = property.Value;
 
-                    // Log property name
-                    Console.WriteLine($"Property Name: {propertyName}");
-
-                    // Check property type and log value
-                    if (propertyValue is TitleProperty titleProperty)
-                    {
-                        foreach (var title in titleProperty.Title)
-                        {
-                            Console.WriteLine($"Title: {title.PlainText}");
-                        }
-                    }
-                    else if (propertyValue is RichTextProperty richTextProperty)
-                    {
-                        foreach (var richText in richTextProperty.RichText)
-                        {
-                            Console.WriteLine($"RichText: {richText.PlainText}");
-                        }
-                    }
-                    else if (propertyValue is NumberProperty numberProperty)
-                    {
-                        Console.WriteLine($"Number: {numberProperty.Number}");
-                    }
-                    else if (propertyValue is SelectProperty selectProperty)
-                    {
-                        Console.WriteLine($"Select: {selectProperty.Select?.Name}");
-                    }
-                    else if (propertyValue is MultiSelectProperty multiSelectProperty)
-                    {
-                        Console.WriteLine($"MultiSelect: {string.Join(", ", multiSelectProperty.MultiSelect.Select(s => s.Name))}");
-                    }
-                    else if (propertyValue is DateProperty dateProperty)
-                    {
-                        Console.WriteLine($"Date: {dateProperty.Date?.Start}");
-                    }
-                    else if (propertyValue is PersonProperty personProperty)
-                    {
-                        Console.WriteLine($"Person: {string.Join(", ", personProperty.Person.Select(p => p.Name))}");
-                    }
-                    else if (propertyValue is FileProperty fileProperty)
-                    {
-                        Console.WriteLine($"File: {string.Join(", ", fileProperty.File.Select(f => f.Name))}");
-                    }
-                    else if (propertyValue is CheckboxProperty checkboxProperty)
-                    {
-                        Console.WriteLine($"Checkbox: {checkboxProperty.Checkbox}");
-                    }
-                    else if (propertyValue is UrlProperty urlProperty)
-                    {
-                        Console.WriteLine($"Url: {urlProperty.Url}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Unknown Property Type");
-                    }
-
-                    Console.WriteLine(); // Add an empty line for readability
+                    _logger.LogInformation("Property Name: {PropertyName}", propertyName);
+                    _logger.LogInformation("Property Type: {PropertyType}", propertyValue);
                 }
             }
         }
