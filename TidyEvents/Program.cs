d4224801f5a6e99f23927e39b0b5dd0c358bcb20
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2024-09-10 12:12:28 +0000

    tidyevent(gdrive): Add base of file parsing

--- TidyEvents/Program.cs
+++ TidyEvents/Program.cs
@@ -10,35 +10,34 @@ var builder = WebApplication.CreateBuilder(args);
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 // Add services to the container.
-builder.Services.AddDbContext<DatabaseContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnection")));
-builder.Services.AddGrpc().AddServiceOptions<TidyBeeEventsService>(options =>
-{
-    options.Interceptors.Add<AuthInterceptor>();
-});
-builder.Services.Configure<AuthInterceptorOption>(builder.Configuration.GetSection("AuthInterceptor"));
-builder.Services.AddHttpClient<AuthInterceptor>();
+// builder.Services.AddDbContext<DatabaseContext>(options =>
+//     options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnection")));
+// builder.Services.AddGrpc().AddServiceOptions<TidyBeeEventsService>(options =>
+// {
+//     options.Interceptors.Add<AuthInterceptor>();
+// });
+// builder.Services.Configure<AuthInterceptorOption>(builder.Configuration.GetSection("AuthInterceptor"));
+// builder.Services.AddHttpClient<AuthInterceptor>();
 
 builder.Services.AddNotionClient(options => {
     options.AuthToken = builder.Configuration.GetSection("Notion:AuthToken").Value;
 });
-builder.Services.AddScoped<NotionFileSyncService>();
+
+// builder.Services.AddScoped<NotionFileSyncService>();
 builder.Services.AddScoped<GoogleDriveSyncService>();
 
 
 var app = builder.Build();
 var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider.GetRequiredService<DatabaseContext>().Database.EnsureCreated();
+// var services = scope.ServiceProvider.GetRequiredService<DatabaseContext>().Database.EnsureCreated();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<TidyBeeEventsService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+// app.MapGrpcService<TidyBeeEventsService>();
+// app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 // Execute the Notion synchronization service
-var notionService = scope.ServiceProvider.GetRequiredService<NotionFileSyncService>();
-await notionService.SyncFilesFromNotionAsync("8b8517a37ee640ddb3fb38275a8d70c2");
+// var notionService = scope.ServiceProvider.GetRequiredService<NotionFileSyncService>();
+// await notionService.SyncFilesFromNotionAsync("8b8517a37ee640ddb3fb38275a8d70c2");
 
-// var googleDriveService = scope.ServiceProvider.GetRequiredService<GoogleDriveSyncService>();
-// await googleDriveService.SyncFilesFromGoogleDriveAsync();
-
-await app.RunAsync();
+var googleDriveService = scope.ServiceProvider.GetRequiredService<GoogleDriveSyncService>();
+await googleDriveService.SyncFilesFromGoogleDriveAsync();
--- TidyEvents/Services/GoogleDriveSyncService.cs
+++ TidyEvents/Services/GoogleDriveSyncService.cs
@@ -3,12 +3,15 @@ using Google.Apis.Services;
 using Google.Apis.Drive.v3;
 using System.IO;
 using System.Threading.Tasks;
+using System.IO.Hashing;
+using System.Text;
 
 namespace TidyEvents.Services
 {
     public class GoogleDriveSyncService
     {
         private readonly ILogger<GoogleDriveSyncService> _logger;
+        private readonly XxHash64 _hasher = new();
 
         public GoogleDriveSyncService(ILogger<GoogleDriveSyncService> logger)
         {
@@ -27,20 +30,33 @@ namespace TidyEvents.Services
             _logger.LogInformation($"GOOGLE DRIVE SYNC STARTED");
 
             var request = service.Files.List();
-            request.Fields = "nextPageToken, files(id, name)";
+            request.Fields = "nextPageToken, files(*)";
             var result = await request.ExecuteAsync();
 
             foreach (var file in result.Files)
             {
-                Console.WriteLine($"File Name: {file.Name}, File ID: {file.Id}");
+                Console.WriteLine($"{file.Name} - {file.Id}");
+                var fileStream = new MemoryStream();
+                var request2 = service.Files.Get(file.Id);
+                await request2.DownloadAsync(fileStream);
+                fileStream.Seek(0, SeekOrigin.Begin);
+                _hasher.Append(fileStream);
+                var fileHash = _hasher.GetHashAndReset();
+                Console.WriteLine($"File Name: {file.Name}, File ID: {file.Id}, xxh64: {Convert.ToBase64String(fileHash)}");
             }
         }
 
+
         private async Task<GoogleCredential> GetCredentialAsync()
         {
-            using var stream = new FileStream("/app/app.json", FileMode.Open, FileAccess.Read);
+            using var stream = new FileStream("app.json", FileMode.Open, FileAccess.Read);
+            var scopes = new[] {
+                DriveService.Scope.Drive,
+                DriveService.Scope.DriveReadonly,
+                DriveService.Scope.DriveMetadataReadonly,
+            };
             var credential = GoogleCredential.FromStream(stream)
-                .CreateScoped(DriveService.Scope.DriveReadonly);
+                .CreateScoped(scopes);
 
             return credential;
         }
