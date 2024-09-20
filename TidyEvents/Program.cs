270d2cdca72463b3e547f4737927305feba3a97d
LucasTesnier <lucas.tesnier@epitech.eu>
2024-09-20 16:54:13 +0200

    Clean TidyEvents

--- TidyEvents/Program.cs
+++ TidyEvents/Program.cs
@@ -35,11 +35,4 @@ var services = scope.ServiceProvider.GetRequiredService<DatabaseContext>().Datab
 app.MapGrpcService<TidyBeeEventsService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-// Execute the Notion synchronization service
-var notionService = scope.ServiceProvider.GetRequiredService<NotionFileSyncService>();
-await notionService.SyncFilesFromNotionAsync("8b8517a37ee640ddb3fb38275a8d70c2");
-
-var googleDriveService = scope.ServiceProvider.GetRequiredService<GoogleDriveSyncService>();
-await googleDriveService.SyncFilesFromGoogleDriveAsync();
-
 await app.RunAsync();
