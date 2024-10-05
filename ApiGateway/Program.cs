573180be3929b45ac56654e82af87475888bd32f
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2024-10-05 16:07:57 +0200

    apigateway: add more authorized cors sources

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -17,7 +17,9 @@ builder.Services.AddCors(options =>
     policy =>
     {
         policy.WithOrigins(
-            "http://0.0.0.0:8080"
+            "http://localhost:8080",
+            "http://prod.tidybee.fr:8080",
+            "http://51.103.93.207:8080"
         );
     });
 });
