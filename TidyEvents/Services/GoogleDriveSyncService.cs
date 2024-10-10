890f13e15677faa7162babebe8452efdaafcd2c6
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2024-10-10 10:30:33 +0200

    tidyevents(gdrive): Sync db via grpc call (#65)
    * tidyevents(gdrive): Sync db via grpc call
    
    * tidyevents(gdrive): Handle file update
    
    * tidyevents(gdrive): remove files not present in drive

--- TidyEvents/Services/GoogleDriveSyncService.cs
+++ TidyEvents/Services/GoogleDriveSyncService.cs
@@ -24,20 +24,33 @@ namespace TidyEvents.Services
             _configuration = configuration;
         }
 
-        public async Task SyncFilesFromGoogleDriveAsync()
+        public async Task SyncFilesFromGoogleDriveAsync(string accessToken)
         {
             var service = new DriveService(new BaseClientService.Initializer
             {
-                HttpClientInitializer = await GetCredentialAsync(),
+                HttpClientInitializer = GetCredentialAsync(accessToken),
                 ApplicationName = "YourAppName"
             });
 
-            _logger.LogInformation($"Retrieving files from Google Drive");
+            _logger.LogInformation($"Retrieving user drives from Google Drive");
+
+            var drive_request = service.Drives.List();
+            drive_request.Fields = "drives(id,name)";
+
+            var drives = await drive_request.ExecuteAsync();
+
+            _logger.LogInformation($"Found {drives.Drives.Count} drives");
+            _logger.LogInformation($"Retrieving files from {drives.Drives[0].Name} (drive id: {drives.Drives[0].Id})");
 
             var request = service.Files.List();
-            request.Fields = "nextPageToken, files(*)";
-          
+            request.Corpora = "drive";
+            request.DriveId = drives.Drives[0].Id;
+            request.SupportsAllDrives = true;
+            request.IncludeItemsFromAllDrives = true;
+            request.Fields = $"nextPageToken,files(*)";
+
             var result = await request.ExecuteAsync();
+            List<string> files_present_in_drive = new();
 
             foreach (var file in result.Files)
             {
@@ -48,23 +61,40 @@ namespace TidyEvents.Services
                 {
                     continue;
                 }
-
                 fileHash = file.Sha256Checksum ?? file.Sha1Checksum ?? file.Md5Checksum ?? await GetFileHashAsync(service, file.Id);
 
-                _logger.LogInformation($"New file: {file.Name}");
+                var existingFile = await _context.Files.FirstOrDefaultAsync(f => f.Name == file.Name);
 
-                _context.Add(new Models.File
+                if (existingFile != null)
                 {
-                    Name = file.Name,
-                    Size = (int)file.Size!,
-                    FileHash = fileHash,
-                    LastModified = DateTime.Parse(file.ModifiedTimeRaw),
-                    MisnamedScore = 'U',
-                    PerishedScore = 'U',
-                    DuplicatedScore = 'U',
-                    GlobalScore = 'U',
-                });
+                    _logger.LogInformation($"Updating file: {file.Name}");
+
+                    existingFile.Size = (int)file.Size!;
+                    existingFile.FileHash = fileHash;
+                    existingFile.LastModified = DateTime.Parse(file.ModifiedTimeRaw);
+                } else {
+                    _logger.LogInformation($"New file: {file.Name}");
+
+                    _context.Add(new Models.File
+                    {
+                        Name = file.Name,
+                        Size = (int)file.Size!,
+                        FileHash = fileHash,
+                        LastModified = DateTime.Parse(file.ModifiedTimeRaw),
+                        MisnamedScore = 'U',
+                        PerishedScore = 'U',
+                        DuplicatedScore = 'U',
+                        GlobalScore = 'U',
+                    });
+                }
+                    files_present_in_drive.Add(file.Name);
             }
+            _logger.LogInformation($"Looking for deleted files");
+            var files_to_delete = await _context.Files.Where(f => !files_present_in_drive.Contains(f.Name)).ToListAsync();
+            _logger.LogInformation($"Found {files_to_delete.Count} files to delete");
+            _context.RemoveRange(files_to_delete);
+
+            _logger.LogInformation($"Saving changes to database");
             await _context.SaveChangesAsync();
             _context.ChangeTracker.Clear();
 
@@ -102,17 +132,20 @@ namespace TidyEvents.Services
             return Convert.ToBase64String(fileHash);
         }
 
-        private async Task<GoogleCredential> GetCredentialAsync()
+        private GoogleCredential GetCredentialAsync(string accessToken)
         {
-            var service_account_keys_path = _configuration.GetSection("GoogleDrive:ServiceAccountKeysPath").Value;
-            using var stream = new FileStream(service_account_keys_path!, FileMode.Open, FileAccess.Read);
-            var scopes = new[] {
-                DriveService.Scope.Drive,
-                DriveService.Scope.DriveReadonly,
-                DriveService.Scope.DriveMetadataReadonly,
-            };
-            var credential = GoogleCredential.FromStream(stream)
-                .CreateScoped(scopes);
+            // Commenting this part because we might need to keep it to use a service account in the future
+            //
+            // var service_account_keys_path = _configuration.GetSection("GoogleDrive:ServiceAccountKeysPath").Value;
+            // using var stream = new FileStream(service_account_keys_path!, FileMode.Open, FileAccess.Read);
+            // var scopes = new[] {
+            //     DriveService.Scope.Drive,
+            //     DriveService.Scope.DriveReadonly,
+            //     DriveService.Scope.DriveMetadataReadonly,
+            // };
+            // var credential = GoogleCredential.FromStream(stream)
+            //     .CreateScoped(scopes);
+            var credential = GoogleCredential.FromAccessToken(accessToken);
 
             return credential;
         }
